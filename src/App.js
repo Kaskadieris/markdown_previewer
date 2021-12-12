d6d3a275516dee7acd1f361ef53d44a913eb82d6
Kaspars Upenieks <36580099+Kaskadieris@users.noreply.github.com>
2021-12-12 14:35:51 +0200

    markdown previewer first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect, useRef } from "react";
+import { marked } from "marked";
 
 function App() {
+  const [text, setText] = useState(null);
+
+  const textarea = useRef(null);
+
+  useEffect(() => {
+    textarea.current.value = `<!-- Headings -->
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+<!-- Links -->
+[Traversy Media](http://www.traversymedia.com)
+
+[Traversy Media](http://www.traversymedia.com "Traversy Media")
+
+
+<!-- Inline Code Block -->
+\`<p>This is a paragraph</p>\`
+
+<!-- Code Blocks -->
+\`\`\`javascript
+  function add(num1, num2) {
+    return num1 + num2;
+  }
+\`\`\`
+
+<!-- UL -->
+* Item 1
+* Item 2
+* Item 3
+  * Nested Item 1
+  * Nested Item 2
+
+<!-- OL -->
+1. Item 1
+1. Item 2
+1. Item 3
+
+
+<!-- Blockquote -->
+> This is a quote
+
+<!-- Images -->
+![Markdown Logo](https://markdown-here.com/img/icon256.png)
+
+
+<!-- Bold -->
+**This text** is bold
+    `;
+
+    let rawMarkup = marked.parse(textarea.current.value);
+
+    setText({ __html: rawMarkup });
+  }, []);
+
+  const handleInput = (e) => {
+    marked.setOptions({
+      gfm: true,
+      breaks: true,
+    });
+
+    let rawMarkup = marked.parse(e.target.value);
+
+    setText({ __html: rawMarkup });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6 my-3">
+          <h5 class="text-success text-center py-4">
+            <strong>Editor:</strong>
+          </h5>
+          <textarea
+            ref={textarea}
+            className="border border-3 border-success rounded "
+            onChange={handleInput}
+            name="markdown_input"
+            id="editor"
+            cols="60"
+            rows="50"
+          ></textarea>
+        </div>
+        <div className="col-lg-6 my-3">
+          <h5 class="text-success text-center py-4 ">
+            <strong>Preview:</strong>
+          </h5>
+          <div
+            className=" border border-3 border-success rounded color-success"
+            id="preview"
+            dangerouslySetInnerHTML={text}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
